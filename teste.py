6079aa00eab8c82b1d77f39532e28bfb3b43c243
itsJoaoVictor <jvgf98@gmail.com>
2022-06-17 17:24:47 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,6 @@
+from model.produto import Produto 
+
+
+#Teste de classe
+notebook = Produto("Notebook", 2000)
+print(notebook)
---
+++ utilidades/helper.py
@@ -0,0 +1,3 @@
+#Função para formatar valor recebido
+def float_str_moeda(valor: float) -> str:
+    return f"R$ {valor:,.2f}" 
